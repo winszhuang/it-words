7460c7a251cdbc9c23089fc9998b49653001059d
wins <win4709hi@gmail.com>
2023-01-14 16:39:35 +0800

    feat: init

---
+++ src/background.ts
@@ -0,0 +1,16 @@
+let active = false;
+
+function makeOrange(color: string): void {
+  document.body.style.backgroundColor = color;
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  active = !active;
+  const color = active ? 'orange' : 'white';
+  console.log('123')
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id ? tab.id : -1 },
+    func: makeOrange,
+    args: [color]
+  }).then();
+});
